20bb8c166ec010d9f101499568c3e16b4195ba8c
Toni Kuschan <toni.kuschan@voange.com>
2025-03-25 21:22:48 +0100

    replacing neru sdk with vcr sdk, fixing index.js processing, QIP not tested properly, but seems working, bumping vcr sdk

--- services/keepalivescheduler/index.js
+++ services/keepalivescheduler/index.js
@@ -1,25 +1,25 @@
-const { neru, Scheduler } = require('neru-alpha');
-const CRONJOB_DEFINITION = '* * * * *';
+const { vcr, Scheduler } = require('@vonage/vcr-sdk')
+const CRONJOB_DEFINITION = '* * * * *'
 
 const deleteKeepAlive = async () => {
   try {
-    const session = neru.createSession();
-    const scheduler = new Scheduler(session);
-    const resp = await scheduler.cancel('keepalive').execute();
-    return resp;
+    const session = vcr.createSession()
+    const scheduler = new Scheduler(session)
+    const resp = await scheduler.cancel('keepalive')
+    return resp
   } catch (e) {
-    console.log(`${e}. Something wrong deleting the keep alive scheduler`);
+    console.log(`${e}. Something wrong deleting the keep alive scheduler`)
   }
-};
+}
 
 const createKeepAlive = async () => {
   try {
-    console.log('creating keep alive scheduler');
-    const session = neru.createSession();
-    const scheduler = new Scheduler(session);
-    let startAtDate = new Date();
-    let endAtDate = new Date();
-    endAtDate.setDate(endAtDate.getHours() + 1); // runs for max 1 hour
+    console.log('creating keep alive scheduler')
+    const session = vcr.createSession()
+    const scheduler = new Scheduler(session)
+    let startAtDate = new Date()
+    let endAtDate = new Date()
+    endAtDate.setDate(endAtDate.getHours() + 1) // runs for max 1 hour
     await scheduler
       .startAt({
         id: 'keepalive',
@@ -29,14 +29,13 @@ const createKeepAlive = async () => {
           cron: CRONJOB_DEFINITION,
         },
       })
-      .execute();
-    return;
+    return
   } catch (e) {
-    console.log(`${e}. Something wrong creating the keep alive scheduler`);
+    console.log(`${e}. Something wrong creating the keep alive scheduler`)
   }
-};
+}
 
 module.exports = {
   createKeepAlive,
   deleteKeepAlive,
-};
+}
--- services/sms/index.js
+++ services/sms/index.js
@@ -1,93 +1,93 @@
 // eslint-disable-next-line no-control-regex
 
-const TEMPLATES_TABLENAME = 'TEMPLATES';
-const axios = require('axios');
-const PROCESSEDFILES_TABLENAME = 'processedfiles';
+const TEMPLATES_TABLENAME = 'TEMPLATES'
+const axios = require('axios')
+const PROCESSEDFILES_TABLENAME = 'processedfiles'
 // column name of csv file that contains the template ID
-const CSV_TEMPLATE_ID_COLUMN_NAME = 'ID_SMSTEXT';
+const CSV_TEMPLATE_ID_COLUMN_NAME = 'ID_SMSTEXT'
 // column name of csv file that contains the phone number of the receiver
-const CSV_PHONE_NUMBER_COLUMN_NAME = 'MOBILTELEFONNUMMER';
+const CSV_PHONE_NUMBER_COLUMN_NAME = 'MOBILTELEFONNUMMER'
 // column name of csv file that contains the ID that will be put into account_ref (together with csv filename)
-const CSV_ID_COLUMN_NAME = 'ID';
+const CSV_ID_COLUMN_NAME = 'ID'
 // column name of csv file that contains the ID that will be put in the client_ref field
-const CSV_CLIENT_REF_COLUMN_NAME = 'VERPFLICHTUNGSNUMMER';
-const isUnicode = (text) => /[^\u0000-\u00ff]/.test(text);
-const rateLimiterService = require('../rateLimiter/index');
-const tps = parseInt(process.env.tps || '30', 10);
-const rateLimitAxios = rateLimiterService.newInstance(tps);
-const utils = require('../../utils');
-const blackListService = require('../blacklist/index');
-// neru tablename for processed filenames
-const { neru, Assets, Scheduler, State } = require('neru-alpha');
-const apikey = process.env.apikey;
-const apiSecret = process.env.apiSecret;
-const api_url = 'https://api.nexmo.com/v1/messages';
-const session = neru.getGlobalSession();
-const globalState = new State(session, `application:f5897b48-9fab-4297-afb5-504d3b9c3296`);
+const CSV_CLIENT_REF_COLUMN_NAME = 'VERPFLICHTUNGSNUMMER'
+const isUnicode = (text) => /[^\u0000-\u00ff]/.test(text)
+const rateLimiterService = require('../rateLimiter/index')
+const tps = parseInt(process.env.tps || '30', 10)
+const rateLimitAxios = rateLimiterService.newInstance(tps)
+const utils = require('../../utils')
+const blackListService = require('../blacklist/index')
+// vcr tablename for processed filenames
+const { vcr, State } = require('@vonage/vcr-sdk')
+const apikey = process.env.apikey
+const apiSecret = process.env.apiSecret
+const api_url = 'https://api.nexmo.com/v1/messages'
+const session = vcr.getGlobalSession()
+const globalState = new State(session, `application:f5897b48-9fab-4297-afb5-504d3b9c3296`)
 
 const sendAllMessages = async (records, filename) => {
-  const csvName = filename.split('send/')[1];
-  const templates = await globalState.hgetall(TEMPLATES_TABLENAME);
+  const csvName = filename.split('send/')[1]
+  const templates = await globalState.mapGetAll(TEMPLATES_TABLENAME)
   const parsedTemplates = Object.keys(templates).map((key) => {
-    const data = JSON.parse(templates[key]);
-    return { ...data };
-  });
+    const data = JSON.parse(templates[key])
+    return { ...data }
+  })
 
   try {
-    let smsCount = 0;
-    let rcsCount = 0;
-    let blackListed = 0;
+    let smsCount = 0
+    let rcsCount = 0
+    let blackListed = 0
 
     const promises = records.map(async (record) => {
       try {
-        const template = parsedTemplates.find((template) => template.id === record[CSV_TEMPLATE_ID_COLUMN_NAME]);
-        let text = template?.text;
-        const rcsTemplate = template?.rcsEnabled;
+        const template = parsedTemplates.find((template) => template.id === record[CSV_TEMPLATE_ID_COLUMN_NAME])
+        let text = template?.text
+        const rcsTemplate = template?.rcsEnabled
 
-        const senderNumber = `${record[`${template?.senderIdField}`]?.replaceAll('+', '')}`;
-        const to = `${record[CSV_PHONE_NUMBER_COLUMN_NAME]?.replaceAll('+', '')}`;
-        const client_ref = record[CSV_ID_COLUMN_NAME];
+        const senderNumber = `${record[`${template?.senderIdField}`]?.replaceAll('+', '')}`
+        const to = `${record[CSV_PHONE_NUMBER_COLUMN_NAME]?.replaceAll('+', '')}`
+        const client_ref = record[CSV_ID_COLUMN_NAME]
 
-        const regexp = /\{\{\s?([\w\d]+)\s?\}\}/g;
+        const regexp = /\{\{\s?([\w\d]+)\s?\}\}/g
         if (text) {
-          const matchArrays = [...text.matchAll(regexp)];
+          const matchArrays = [...text.matchAll(regexp)]
           matchArrays.forEach((array) => {
-            text = text.replaceAll(array[0], record[`${array[1]}`]);
-          });
+            text = text.replaceAll(array[0], record[`${array[1]}`])
+          })
         }
 
-        const client_ref_obj = { client_ref: client_ref };
+        const client_ref_obj = { client_ref: client_ref }
 
-        const result = await sendSmsOrRcs(senderNumber, to, text, api_url, client_ref, csvName, rateLimitAxios, rcsTemplate);
+        const result = await sendSmsOrRcs(senderNumber, to, text, api_url, client_ref, csvName, rateLimitAxios, rcsTemplate)
 
         // Increment SMS or RCS count based on the channel
-        if (result.channel === 'sms') smsCount++;
-        if (result.channel === 'rcs') rcsCount++;
-        if (result.channel === 'blacklist') blackListed++;
+        if (result.channel === 'sms') smsCount++
+        if (result.channel === 'rcs') rcsCount++
+        if (result.channel === 'blacklist') blackListed++
 
-        return Promise.resolve(Object.assign({}, result, client_ref_obj));
+        return Promise.resolve(Object.assign({}, result, client_ref_obj))
       } catch (error) {
-        return Promise.reject(error);
+        return Promise.reject(error)
       }
-    });
+    })
 
-    const results = await Promise.all(promises);
+    const results = await Promise.all(promises)
 
     // Add SMS and RCS counts to results summary
-    results.push({ smsCount, rcsCount, blackListed });
+    results.push({ smsCount, rcsCount, blackListed })
 
-    return results;
+    return results
   } catch (error) {
-    console.error(error);
-    return error;
+    console.error(error)
+    return error
   }
-};
+}
 
 const sendOptOutRcs = async (senderNumber, to) => {
   const headers = {
     Authorization: `Bearer ${utils.generateToken()}`, // Use the JWT token parameter
     'Content-Type': 'application/json',
-  };
+  }
   const body = {
     message_type: 'text',
     from: senderNumber,
@@ -96,32 +96,32 @@ const sendOptOutRcs = async (senderNumber, to) => {
     text: 'Sie haben sich erfolgreich abgemeldet und werden keine RCS Nachrichten mehr zu diesem Vorgang erhalten.',
     sms: { encoding_type: 'auto' },
     client_ref: `opt-out`,
-  };
+  }
   try {
-    const response = await axios.post(api_url, body, { headers });
+    const response = await axios.post(api_url, body, { headers })
     return {
       ...response.data,
       // Include the channel in the returned object
-    };
+    }
   } catch (error) {
-    console.error(error.response.data);
-    return { ...error.response.data, channel };
+    console.error(error.response.data)
+    return { ...error.response.data, channel }
     // return Promise.reject(error);
   }
-};
+}
 
 const sendSmsOrRcs = async (senderNumber, to, text, apiUrl, campaignName, csvName, axios, rcsTemplate) => {
-  let channel = 'sms'; // Default channel is SMS
-  let from = senderNumber || 'test';
+  let channel = 'sms' // Default channel is SMS
+  let from = senderNumber || 'test'
   const headers = {
     Authorization: `Bearer ${utils.generateToken()}`, // Use the JWT token parameter
     'Content-Type': 'application/json',
-  };
+  }
 
   if (rcsTemplate) {
-    const isRcsSupported = await utils.checkRCS(to);
-    channel = isRcsSupported ? 'rcs' : 'sms';
-    from = isRcsSupported ? utils.rcsAgent : from;
+    const isRcsSupported = await utils.checkRCS(to)
+    channel = isRcsSupported ? 'rcs' : 'sms'
+    from = isRcsSupported ? utils.rcsAgent : from
   }
 
   const body = {
@@ -132,34 +132,34 @@ const sendSmsOrRcs = async (senderNumber, to, text, apiUrl, campaignName, csvNam
     text: text,
     sms: { encoding_type: 'auto' },
     client_ref: `${campaignName}-${csvName}`,
-  };
-  const isBlackListed = await blackListService.isBlackListed(to);
+  }
+  const isBlackListed = await blackListService.isBlackListed(to)
   if (isBlackListed) {
     return {
       message_id: 'Blacklisted number - User sent STOP',
       channel: 'blacklist',
-    };
+    }
   }
 
   try {
-    const response = await axios.post(apiUrl, body, { headers });
+    const response = await axios.post(apiUrl, body, { headers })
     return {
       ...response.data,
       channel, // Include the channel in the returned object
-    };
+    }
   } catch (error) {
-    console.error(error.response.data);
+    console.error(error.response.data)
     if (error.response != null && error.response.status === 429) {
-      console.log('Too many requests (429), retrying...');
+      console.log('Too many requests (429), retrying...')
       // return sendSmsOrRcs(to, text, apiUrl, campaignName, csvName, axios, rcsTemplate);
     }
-    return { ...error.response.data, channel };
+    return { ...error.response.data, channel }
     // return Promise.reject(error);
   }
-};
+}
 
 module.exports = {
   sendSmsOrRcs,
   sendAllMessages,
   sendOptOutRcs,
-};
+}
